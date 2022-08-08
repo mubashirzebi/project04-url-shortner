5ed579f6c9822d77c0a9a7c88cd0151cfc066e5c
mubashirzebi <mubashshirzebi.com>
2022-08-08 20:46:49 +0530

    update redis

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,29 +2,21 @@
 const urlModel = require('../models/urlModel')
 const randomString = require('randomstring');
 const redis = require("redis");
-const { promisify } = require("util");
 
 
 //=======================================Connect to redis=============================================//
 
-const redisClient = redis.createClient(
-    12542,
-    "redis-12542.c9.us-east-1-4.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
-);
-
-redisClient.auth("JTA6xJq8bKzz0sJNGqNGYJa5lpVhKn5h", function (err) {
-    if (err) throw err;
+const client = redis.createClient({
+    url: "redis://default:mJsrV4ebvRJIpn0ffI1NV0rzOPulQ2qS@redis-16938.c74.us-east-1-4.ec2.cloud.redislabs.com:16938"
 });
 
-redisClient.on("connect", async function () {
-    console.log("Connected to Redis..");
+client.connect()
+.then(()=> {
+console.log("redis Connected")
+})
+.catch( err => {
+    console.log('Error ' + err);
 });
-
-//Connection setup for redis
-
-const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
-const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 //==============================================POST /url/shorten=======================================================//
@@ -46,7 +38,7 @@ const urlShortner = async function (req, res) {
             return res.status(400).send({ status: false, message: "Url is invalid", })
         }
 
-        const checkLongUrlInRedis = await GET_ASYNC(`${longUrl}`)
+        const checkLongUrlInRedis = await client.get(`${longUrl}`)
 
         const redisOutPut = JSON.parse(checkLongUrlInRedis)
 
@@ -58,7 +50,7 @@ const urlShortner = async function (req, res) {
 
         if (checkDuplicateLongUrlInDB) {
 
-            await SETEX_ASYNC(`${longUrl}`, 60 * 60, JSON.stringify(checkDuplicateLongUrlInDB))
+            await client.setEx(`${longUrl}`, 60 * 60, JSON.stringify(checkDuplicateLongUrlInDB))
 
             return res.status(201).send({ status: true, data: checkDuplicateLongUrlInDB })
         }
@@ -101,7 +93,7 @@ const getUrl = async function (req, res) {
 
         const urlCode = req.params.urlCode
 
-        const findUrlCodeInRedis = await GET_ASYNC(`${urlCode}`)
+        const findUrlCodeInRedis = await client.get(`${urlCode}`)
 
         if (findUrlCodeInRedis) {
             return res.status(302).redirect(findUrlCodeInRedis)
@@ -113,7 +105,7 @@ const getUrl = async function (req, res) {
             return res.status(404).send({ status: false, message: "urlCode is not found in DB..." })
         }
 
-        await SETEX_ASYNC(`${urlCode}`, 60 * 60, findUrlCodeInDB.longUrl)
+        await client.setEx(`${urlCode}`, 60 * 60, findUrlCodeInDB.longUrl)
 
         return res.status(302).redirect(findUrlCodeInDB.longUrl)
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const  mongoose  = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose.connect("mongodb+srv://project-04-urlShortner:dFfYOCdrsu2KLlf8@cluster0.j1yrl.mongodb.net/project4-Group67-Database?retryWrites=true&w=majority", {
